494cd50d2f61195c00752359767c862cdb755a29
ShaiFisher1 <74471077+ShaiFisher1@users.noreply.github.com>
2022-05-27 19:03:00 +0300

    Delete WebApplicationAP2EX2 directory

--- WebApplicationAP2EX2/WebApplicationAP2EX2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplicationAP2EX2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
