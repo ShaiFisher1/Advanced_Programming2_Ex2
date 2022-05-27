f72032196e141d1b6d0a84c9611179fb796ba5d5
ShaiFisher1 <fake@fake.com>
2022-05-27 03:13:22 +0300

    adding service to contactcontroller

--- WebApplication1/WebApplication1/Controllers/MessagesController.cs
+++ WebApplication1/WebApplication1/Controllers/MessagesController.cs
@@ -10,7 +10,7 @@ using WebApplication1;
 
 namespace WebApplication1.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/contacts")]
     [ApiController]
     public class MessagesController : ControllerBase
     {
@@ -21,88 +21,184 @@ namespace WebApplication1.Controllers
             _context = context;
         }
 
-        // GET: api/Messages
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Message>>> GetMessages()
+        public class NewMessageObj
         {
-            return await _context.Messages.ToListAsync();
+            public string? userName { get; set; }
+            public string? content { get; set; }
         }
 
-        // GET: api/Messages/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Message>> GetMessage(int id)
-        {
-            var message = await _context.Messages.FindAsync(id);
 
-            if (message == null)
+        [HttpGet("{id}/messages")]
+        public async Task<ActionResult<IEnumerable<Message>>> getMessages(string userName, string id)
+        {
+            //User currentUser = _context.Users.Where(u => u.id == userName).FirstOrDefault();
+            Contact currentcontact = _context.Contacts.Where(e => e.contactid == id && e.username == userName).FirstOrDefault();
+            if (currentcontact == null)
             {
                 return NotFound();
             }
-
-            return message;
+            //Chat wantedChat = _context.Chat.Where(c => c.userid == currentUser.id && c.contactid == id || c.userid == id && c.contactid==currentUser.id).FirstOrDefault();
+            Chat wantedChat = _context.Chat.Where(c => c.userid == userName && c.contactid == id).FirstOrDefault();
+            if (wantedChat == null)
+            {
+                return Ok(new List<Message>());
+            }
+            var response = await _context.Messages.Where(e => e.ChatId == wantedChat.id).ToListAsync();
+            return response;
         }
 
-        // PUT: api/Messages/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMessage(int id, Message message)
+        // post create a new message between contact and current user --- id is the contact
+        [HttpPost("{id}/messages")]
+        public async Task<ActionResult<Message>> PostMessage(string id, [FromBody] NewMessageObj newmsgobj)
         {
-            if (id != message.id)
+            var currentContact = _context.Contacts.Where(e => e.contactid == id && e.username == newmsgobj.userName).FirstOrDefault();
+            if (currentContact == null)
             {
-                return BadRequest();
+                return NotFound();
             }
-
-            _context.Entry(message).State = EntityState.Modified;
-
-            try
+            DateTime msgDate = DateTime.Now; // todo string? 
+            int newChatId1, newChatId2;
+            int followingId;
+            // server same check
+            if (_context.Users.Where(e => e.id == newmsgobj.userName).FirstOrDefault().server == currentContact.server)
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MessageExists(id))
+                if (_context.Chat.Where(c => c.userid == newmsgobj.userName && c.contactid == id).FirstOrDefault() == null)
                 {
-                    return NotFound();
+                    if (_context.Chat.Count() == 0)
+                    {
+                        newChatId1 = 1;
+                    }
+                    else
+                    {
+                        newChatId1 = _context.Chat.Max(c => c.id) + 1;
+                    }
+                    newChatId2 = newChatId1 + 1;
+                    Chat chat1 = new Chat() { id = newChatId1, contactid = currentContact.contactid, userid = newmsgobj.userName };
+                    Chat chat2 = new Chat() { id = newChatId2, contactid = newmsgobj.userName, userid = currentContact.contactid };
+                    _context.Chat.Add(chat1);
+                    _context.Chat.Add(chat2);
+                    await _context.SaveChangesAsync();
+                }
+
+                else
+                {
+                    newChatId1 = _context.Chat.Where(c => c.userid == newmsgobj.userName && c.contactid == id).FirstOrDefault().id;
+                    newChatId2 = _context.Chat.Where(c => c.userid == id && c.contactid == newmsgobj.userName).FirstOrDefault().id;
+
+                }
+
+                if (_context.Messages.Count() != 0)
+                {
+                    followingId = _context.Messages.Max(e => e.id) + 1;
                 }
                 else
                 {
-                    throw;
+                    followingId = 1;
                 }
+                Message newmsg1 = new Message() { id = followingId, content = newmsgobj.content, created = msgDate, sent = true, ChatId = newChatId1 };
+                Message newmsg2 = new Message() { id = followingId + 1, content = newmsgobj.content, created = msgDate, sent = false, ChatId = newChatId2 };
+                currentContact.last = newmsg1.content;
+                currentContact.lastdate = newmsg1.created;
+                Contact userNameAsContact = _context.Contacts.Where(c => c.username == currentContact.contactid && c.contactid == newmsgobj.userName).FirstOrDefault();
+                userNameAsContact.last = newmsg2.content;
+                currentContact.lastdate = newmsg2.created;
+                _context.Messages.Add(newmsg1);
+                _context.Messages.Add(newmsg2);
+                ;
             }
-
-            return NoContent();
-        }
-
-        // POST: api/Messages
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Message>> PostMessage(Message message)
-        {
-            _context.Messages.Add(message);
+            else
+            {
+                int newChatId;
+                if (_context.Chat.Where(c => c.userid == newmsgobj.userName && c.contactid == id).FirstOrDefault() == null)
+                {
+                    if (_context.Chat.Count() == 0)
+                    {
+                        newChatId = 1;
+                    }
+                    else
+                    {
+                        newChatId = _context.Chat.Max(c => c.id) + 1;
+                    }
+                    int updatedChatId = newChatId;
+                    Chat chat = new Chat() { id = updatedChatId, contactid = currentContact.contactid, userid = newmsgobj.userName };
+                    _context.Chat.Add(chat);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    newChatId = _context.Chat.Where(c => c.userid == newmsgobj.userName && c.contactid == id).FirstOrDefault().id;
+                }
+                if (_context.Messages.Count() != 0)
+                {
+                    followingId = _context.Messages.Max(e => e.id) + 1;
+                }
+                else
+                {
+                    followingId = 1;
+                }
+                Message newmsg = new Message() { id = followingId, content = newmsgobj.content, created = msgDate, sent = true, ChatId = newChatId };
+                currentContact.last = newmsg.content;
+                currentContact.lastdate = newmsg.created;
+                _context.Messages.Add(newmsg);
+            }
             await _context.SaveChangesAsync();
+            return StatusCode(201);
 
-            return CreatedAtAction("GetMessage", new { id = message.id }, message);
+
         }
 
-        // DELETE: api/Messages/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMessage(int id)
+        // get a specific message details --- id is contact and id2 is message Id
+        [HttpGet("{id}/messages/{id2}")]
+        public async Task<ActionResult<Message>> GetMessage(string userName, string id, int id2)
         {
-            var message = await _context.Messages.FindAsync(id);
-            if (message == null)
+            User currentUser = _context.Users.Where(u => u.id == userName).FirstOrDefault();
+            Contact currentContact = _context.Contacts.Where(e => e.contactid == id && e.username == userName).FirstOrDefault();
+            Chat wantedChat = _context.Chat.Where(c => c.userid == currentUser.id && c.contactid == id).FirstOrDefault();
+            return _context.Messages.Where(m => m.ChatId == wantedChat.id && m.id == id2).FirstOrDefault();
+        }
+
+        // put update a message --- id is contact and id2 is message Id
+
+        [HttpPut("{id}/Messages/{id2}")]
+        public async Task<IActionResult> PutMessage(string id, int id2, [FromBody] NewMessageObj newmsgobj)
+        {
+            Message message = _context.Messages.Where(m => m.id == id2).FirstOrDefault();
+            Chat currentChat = _context.Chat.Where(c => c.id == message.ChatId).FirstOrDefault();
+            Contact currentContact = _context.Contacts.Where(e => e.contactid == id && e.username == newmsgobj.userName).FirstOrDefault();
+            if (currentChat.contactid != currentContact.contactid) // todo change if we change primary keys of contact
+            {
+                return BadRequest();
+            }
+            if (message != null)
+            {
+                message.content = newmsgobj.content;
+                message.created = DateTime.Now;
+                await _context.SaveChangesAsync();
+                return StatusCode(204);
+            }
+            else
             {
                 return NotFound();
             }
+        }
 
-            _context.Messages.Remove(message);
+        // delete a message --- id is contact and id2 is message Id
+        [HttpDelete("{id}/Messages/{id2}")]
+        public async Task<ActionResult> DeleteMessage([FromBody] string userName, string id, int id2)
+        {
+            User currentUser = _context.Users.Where(u => u.id == userName).FirstOrDefault();
+            Contact currentContact = _context.Contacts.Where(e => e.contactid == id && e.username == userName).FirstOrDefault();
+            Chat wantedChat = _context.Chat.Where(c => c.userid == currentUser.id && c.contactid == id).FirstOrDefault();
+            Message currentMessage = _context.Messages.Where(m => m.ChatId == wantedChat.id && m.id == id2).FirstOrDefault();
+            if (currentMessage == null)
+            {
+                return NotFound();
+            }
+            _context.Messages.Remove(currentMessage);
             await _context.SaveChangesAsync();
-
             return NoContent();
         }
 
-        private bool MessageExists(int id)
-        {
-            return _context.Messages.Any(e => e.id == id);
-        }
+
     }
 }
